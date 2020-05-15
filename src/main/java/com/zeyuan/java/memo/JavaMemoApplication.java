972af5d2ce8477b0a2f54738f74db1531e965f10
zeyuan <jiangzeyuan@gmail.com>
2020-05-16 02:29:41 +0800

    first commit

---
+++ src/main/java/com/zeyuan/java/memo/JavaMemoApplication.java
@@ -0,0 +1,13 @@
+package com.zeyuan.java.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeyuan/java/memo/JavaMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeyuan.java.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
